78f87412396926205a2c193425d17c50e7c9d6bc
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2025-05-01 11:38:40 +0200

    Remove fake MemberGroups.ListMemberAssociationsAsync

--- src/Akiles.ApiClient/MemberGroups/IMemberGroups.cs
+++ src/Akiles.ApiClient/MemberGroups/IMemberGroups.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using Akiles.ApiClient.Members;
-using Refit;
+﻿using Refit;
 
 namespace Akiles.ApiClient.MemberGroups;
 
@@ -32,7 +30,7 @@ public interface IMemberGroups
     );
 
     [Post("/member_groups")]
-    Task<MemberGroup> CreateMemberAsync(
+    Task<MemberGroup> CreateMemberGroupAsync(
         MemberGroupInit member,
         CancellationToken cancellationToken = default
     );
@@ -45,77 +43,8 @@ public interface IMemberGroups
     );
 
     [Delete("/member_groups/{memberGroupId}")]
-    Task<MemberGroup> DeleteMemberAsync(
+    Task<MemberGroup> DeleteMemberGroupAsync(
         string memberGroupId,
         CancellationToken cancellationToken = default
     );
-
-    [Get("/members")]
-    internal Task<PagedList<Member>> ListMembersAsync(
-        string? cursor,
-        int? limit,
-        Sort<Member>? sort,
-        ListMembersFilter? filter = null,
-        MembersExpand expand = MembersExpand.None,
-        CancellationToken cancellationToken = default
-    );
-}
-
-public static class MemberGroupsExtensions
-{
-    public static async IAsyncEnumerable<MemberGroupAssociation> ListMemberAssociationsAsync(
-        this IMemberGroups memberGroups,
-        string memberGroupId,
-        MemberGroupsExpand expand = MemberGroupsExpand.None,
-        [EnumeratorCancellation] CancellationToken cancellationToken = default
-    )
-    {
-        var page = await memberGroups.ListMembersAsync(
-            cursor: null,
-            limit: Constants.DefaultPaginationLimit,
-            sort: null,
-            filter: null,
-            expand: MembersExpand.GroupAssociations,
-            cancellationToken
-        );
-
-        while (!cancellationToken.IsCancellationRequested)
-        {
-            foreach (var member in page.Data)
-            {
-                if (member.GroupAssociations is null)
-                {
-                    continue;
-                }
-
-                foreach (
-                    var association in member.GroupAssociations.Where(x =>
-                        x.MemberGroupId == memberGroupId
-                    )
-                )
-                {
-                    if (expand.HasFlag(MemberGroupsExpand.Member))
-                    {
-                        association.Member = member;
-                    }
-
-                    yield return association;
-                }
-            }
-
-            if (!page.HasNext)
-            {
-                break;
-            }
-
-            page = await memberGroups.ListMembersAsync(
-                cursor: page.CursorNext,
-                limit: Constants.DefaultPaginationLimit,
-                sort: null,
-                filter: null,
-                expand: MembersExpand.GroupAssociations,
-                cancellationToken
-            );
-        }
-    }
 }
