00e63a58b395a184a2934fad14507615abaf9471
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2025-05-12 14:47:19 +0200

    Fix event list filter

--- src/Akiles.ApiClient/Events/ListEventsFilter.cs
+++ src/Akiles.ApiClient/Events/ListEventsFilter.cs
@@ -1,9 +1,15 @@
-﻿namespace Akiles.ApiClient.Events;
+﻿using Refit;
+
+namespace Akiles.ApiClient.Events;
 
 public record ListEventsFilter
 {
     public EventVerb? Verb { get; set; }
+
+    [AliasAs("subject.")]
     public ListEventsSubjectFilter? Subject { get; set; }
+
+    [AliasAs("object.")]
     public ListEventsObjectFilter? Object { get; set; }
     public RangeFilter<DateTimeOffset>? CreatedAt { get; set; }
 }
