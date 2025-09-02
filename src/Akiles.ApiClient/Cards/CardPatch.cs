b363975dd54d5bf36873ac5b228ce7c96bdd3ede
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2025-09-02 20:15:58 +0200

    Add name to card patch

--- src/Akiles.ApiClient/Cards/CardPatch.cs
+++ src/Akiles.ApiClient/Cards/CardPatch.cs
@@ -4,6 +4,8 @@ namespace Akiles.ApiClient.Cards;
 
 public record CardPatch
 {
+    public string? Name { get; set; }
+
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public Dictionary<string, string?>? Metadata { get; set; }
 }
